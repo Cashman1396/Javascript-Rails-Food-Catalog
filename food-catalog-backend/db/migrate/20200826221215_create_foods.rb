5286a49ce5af63819e67c9b7b587ea16aa090507
chris_ashman <chrisashman96@gmail.com>
2020-08-26 19:44:38 -0400

    added models

--- food-catalog-backend/db/migrate/20200826221215_create_foods.rb
+++ food-catalog-backend/db/migrate/20200826221215_create_foods.rb
@@ -1,6 +1,13 @@
 class CreateFoods < ActiveRecord::Migration[6.0]
   def change
     create_table :foods do |t|
+      t.string :name
+      t.decimal :cost, default: "0.00"
+      t.text :description
+      t.string :image_url
+      t.integer :catalog_id
+
+      t.timestamps null: false
     end
   end
 end
